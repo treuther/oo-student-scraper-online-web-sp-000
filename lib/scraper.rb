27478ddaaf888c3a2d29438bac6c6d9cca888695
Tracy Reuther <tracyreuther@gmail.com>
2019-07-23 00:54:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
 require 'open-uri'
 require 'pry'
 
+#Twitter, linkedin, github, blog, profile_quote, and bio
 class Scraper
 
+#Responsible for scraping the index page that lists all of the students
+
+#Takes in URL as argument
+#Uses Nokogiri + Open-URI to access page
+#Return value should be an array of hashes - ea hash represents a single student
+#keys should be :name, :location and :profile_url
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = open (index_url)
+    index = Nokogiri::HTML(html)
+    index.css(".student-text-card"). each do |student|
+      student_details = {}
+      student_details[:name] = student.css("h4.student-name").text
+      student_details[:location] = student.css("p.student-location").text
+      profile_path = student.css("a").attribute("href").value
+      student_details[:profile_url] = '.fixtures/student-site/' + profile_path
+      students << student_details
+    end
+      students
   end
 
+#Responsible for scraping an indiviual student's profile page to get student info
+#Takes in a student's profile URL
+#Uses Nokogiri + Open-URI to access page
+#Return value should be a hash with key/value pairs that describe an indivual student
+#Some students have twitter while others don't
   def self.scrape_profile_page(profile_url)
-    
+
+
+
   end
 
 end
 
+#index.css(".student-text-card").first
+#index.css("h4.student-name").first.text
+#index.css("p.student-location").first.text
+#index.css("a").attribute("href").value
+#'.fixtures/student-site/'
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,32 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
+#Takes in an argument of a hash, uses metaprogramming to assign attributes + values
+#Use the send method!
+#Adds the new student to the @@al array of students with push self
   def initialize(student_hash)
-    
+
   end
 
+#Takes in an array of hashes, then iterate over the array and create a new individual student
   def self.create_from_collection(students_array)
-    
+
   end
 
+#Takes in a hash of whose key/value pairs describe additional attributes of an indivual student
+#Iterates over given Scraper.scrape_profile_page, uses metaprogramming to assign attributes
+#and values.
+#Use the send method
+#The return value should be the student itself. use the self keyword.
   def add_student_attributes(attributes_hash)
-    
+
   end
 
+#Returns the contents of @@all array
   def self.all
-    
+
   end
 end
-
