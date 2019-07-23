c6abf04004ac1f84a6562bd3cf4b9f4d5b65c4c0
Tracy Reuther <tracyreuther@gmail.com>
2019-07-23 22:32:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,8 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      profile_path = student.css("a").attribute("href").value.slice!(".fixtures/student-site/")
+      profile_path = student.css("a").attribute("href").value
+      profile_path.slice!(".fixtures/student-site/")
       student_details[:profile_url] = profile_path
       students << student_details
     end
