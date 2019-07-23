249dd8f868064b952a35c604a48cf5452713019b
Tracy Reuther <tracyreuther@gmail.com>
2019-07-23 23:31:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,12 +14,13 @@ class Scraper
     students = []
     html = open (index_url)
     index = Nokogiri::HTML(html)
-    index.css(".student-text-card"). each do |student|
+    index.css("div.student-card"). each do |student|
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
-      student_details[:profile_url] = '.fixtures/student-site/' + profile_path
+      profile_path.slice!(".fixtures/student-site/")
+      student_details[:profile_url] = profile_path
       students << student_details
     end
       students
@@ -31,9 +32,25 @@ class Scraper
 #Return value should be a hash with key/value pairs that describe an indivual student
 #Some students have twitter while others don't
   def self.scrape_profile_page(profile_url)
-
-
-
+    student_profile = {}
+    html = open (profile_url)
+    profile = Nokogiri::HTML(html)
+# binding.pry
+    #social
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
+    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+    student_profile
   end
 
 end
@@ -43,3 +60,7 @@ end
 #index.css("p.student-location").first.text
 #index.css("a").attribute("href").value
 #'.fixtures/student-site/'
+
+#Social container: profile.css(".social-icon-container").first
+#Profile quote: profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+#Bio: profile.css("div.main-wrapper.profile .description-holder p").text
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
@@ -8,12 +9,16 @@ class Student
 #Use the send method!
 #Adds the new student to the @@al array of students with push self
   def initialize(student_hash)
-
+    student_hash.each {|k, v| self.send(("#{k}="), v)}
+    @@all << self
   end
 
-#Takes in an array of hashes, then iterate over the array and create a new individual student
+#Takes in an array of hashes, then iterates over the array and creates a new individual student
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_info|
+      self.new(student_info)
+      # binding.pry
+    end
   end
 
 #Takes in a hash of whose key/value pairs describe additional attributes of an indivual student
@@ -22,11 +27,14 @@ class Student
 #Use the send method
 #The return value should be the student itself. use the self keyword.
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |k,v|
+      self.send(("#{k}="), v)
   end
+  self
+end
 
 #Returns the contents of @@all array
   def self.all
-
+    @@all
   end
 end
